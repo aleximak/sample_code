a617b6c2ffcfaa2807a4bc74de5ae563f6d6085a
Alex Afshari <alexafshari@Alexs-MacBook-Pro.local>
2018-11-01 12:34:59 -0700

    three sample pure html websites I created from scratch while working for Law Promo/Web Promo

---
+++ extent/js/backToTop.js
@@ -0,0 +1,20 @@
+
+
+
+// Smooth Scroll
+var amountScrolled = 400;
+
+$(window).scroll(function() {
+  if ( $(window).scrollTop() > amountScrolled ) {
+    $('a.back-to-top').fadeIn('slow');
+  } else {
+    $('a.back-to-top').fadeOut('slow');
+  }
+});
+
+$('a.back-to-top').click(function() {
+  $('html, body').animate({
+    scrollTop: 0
+  }, 1000);
+  return false;
+});
